6e39c5e61df4dd7527f3beae74a79d3a90010b91
suvi1995 <115064247+suvi1995@users.noreply.github.com>
2024-09-08 23:05:57 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,7 @@
 sudo apt update 
 sudo apt install docker.io
+sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
+sudo chmod 700 /var/run/docker.sock
+docker login -u suvitha
