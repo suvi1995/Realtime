2c9f121f30464767e20bf50683eefa0ddd5df093
suvi1995 <115064247+suvi1995@users.noreply.github.com>
2024-09-23 14:23:09 +0530

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -10,18 +10,18 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt install -y apt-transport-https ca-certificates curl gpg
 sudo apt install gpg
 sudo mkdir -p -m 755 /etc/apt/keyrings
-
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
-sudo apt-mark hold -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
 in masternode:
  sudo kubeadm init --pod-network-cidr=192.168.0.0/16
  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
  sudo chown $(id -u):$(id -g) $HOME/.kube/config
- kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
+ 
   in workernode:
  kubeadm join 172.31.24.246:6443 --token f0yit9.7yw62nbabh0k1rhi \
         --discovery-token-ca-cert-hash sha256:39bd3c2e5738f4c922df723ce0262536527651abed27028d30b75eb5aa96ee3c
